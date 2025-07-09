cf33cb33dd2575b6261b958ab0dd5e221f86530d
Davide Garberi <dade.garberi@gmail.com>
2025-07-10 00:53:19 +0200

    SyUtf8Icon: Simplify the utf8 icon conversion

--- src/components/SiyuanTheme/SyUtf8Icon.vue
+++ src/components/SiyuanTheme/SyUtf8Icon.vue
@@ -24,25 +24,8 @@ const props = defineProps<{
 
 const utf8Icon = computed(() => {
   if (!props.utf8Code) return ''
-  
-  // If it's already a valid UTF8 character, return it directly
-  if (props.utf8Code.length === 1) {
-    return props.utf8Code
-  }
-  
-  // If it's a unicode code point (like "1f4c4"), convert it to character
-  if (/^[0-9a-fA-F]+$/.test(props.utf8Code)) {
-    try {
-      const codePoint = parseInt(props.utf8Code, 16)
-      return String.fromCodePoint(codePoint)
-    } catch (e) {
-      console.warn('Invalid UTF8 code:', props.utf8Code)
-      return ''
-    }
-  }
-  
-  // If it's already a character or emoji, return as is
-  return props.utf8Code
+
+  return String.fromCodePoint(...props.utf8Code.split('-').map(p => parseInt(p, 16)));
 })
 </script>
 
